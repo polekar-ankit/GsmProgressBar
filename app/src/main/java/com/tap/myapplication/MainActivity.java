97fa53727b8e429e31f0f0c4bb04d1fe19fa4185
polekar.ankit <polekar.ankit@gmail.com>
2019-05-08 10:07:09 +0530

    Initial commit

---
+++ app/src/main/java/com/tap/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tap.myapplication;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
